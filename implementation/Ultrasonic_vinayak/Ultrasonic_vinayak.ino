3870bdee2ad47c9918f9611908ae9e7722a082fb
Vinayakkoshti <vinayakkoshti111@gmail.com>
2021-09-05 14:29:08 +0530

    added level feature in tank

--- implementation/Ultrasonic_vinayak/Ultrasonic_vinayak.ino
+++ implementation/Ultrasonic_vinayak/Ultrasonic_vinayak.ino
@@ -5,57 +5,26 @@
 float calibrate;  //to set fix distance (Threshold)
 int Person_count = 0;  // to count people in room  
 int Full_Tank = 10; //setting full tank level distance 10cm
+int Mid_Tank = 100; // tank filed at middle level
+int Empty_Tank = 200;// tank is empty
+
 long T;
 float distanceCM;
 
 void Ultra_Sonic_Distance();
-int Ultra_Sonic_Count();
 
 void setup() {
-Serial.begin(115200);
-pinMode(trigger,OUTPUT);
-pinMode(echo,INPUT);
-
-//Set first distance as a refference distnce to calculate object count
-  digitalWrite(trigger,LOW);
-  delay(1);
-  digitalWrite(trigger,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger,LOW);
-  T=pulseIn(echo,HIGH);
-  calibrate = T * 0.034;
-  calibrate = calibrate / 2;
-  Serial.print("calibrate : ");
-  Serial.println(calibrate);
-
+  
+  Serial.begin(115200);
+  pinMode(trigger,OUTPUT);
+  pinMode(echo,INPUT);
+   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //Ultra_Sonic_Count();
   Ultra_Sonic_Distance();
   delay(1000);
-}
-
-int Ultra_Sonic_Count()
-{
-  digitalWrite(trigger,LOW);
-  delay(1);
-  digitalWrite(trigger,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger,LOW);
-  T=pulseIn(echo,HIGH);
-  distanceCM =T * 0.034;
-  distanceCM = distanceCM / 2;
-  Serial.print("Distance in CM : ");
-  Serial.println(distanceCM);
-  
-  if(distanceCM < calibrate)
-  {
-    Person_count++;
-    Serial.print("Number of people in Room : ");
-    Serial.println(Person_count);
-  } 
 }
 
 void Ultra_Sonic_Distance()
@@ -76,5 +45,20 @@ void Ultra_Sonic_Distance()
       Serial.println("Tank is Full turning off switch");
       digitalWrite(LED,HIGH); // turn off switch
       //thingspeak
+  }
+  else
+  if(distanceCM <= Mid_Tank)
+  {
+      Serial.println("Tank is Mid Level turning ON switch");
+      digitalWrite(LED,HIGH); // turn off switch
+      //thingspeak
+  }
+  else
+  if(distanceCM <= Empty_Tank)
+  {
+      Serial.println("Tank is Empty turning ON switch");
+      digitalWrite(LED,HIGH); // turn off switch
+      //thingspeak
   } 
+  
 }
