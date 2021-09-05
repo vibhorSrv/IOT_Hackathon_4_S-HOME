e73d1c1fb14f14928cfbcd763457845e0620c881
Vibhor Srivastava <vibhor.srv@gmail.com>
2021-09-05 19:08:33 +0530

    DHT11 code

--- implementation/MergedCode/MergedCode.ino
+++ implementation/MergedCode/MergedCode.ino
@@ -1,4 +1,5 @@
 /* Essentials */
+#include<DHT.h>
 #define SERIAL_BAUD_RATE 115200
 
 /******************************************************************/
@@ -6,6 +7,8 @@
 /* Sensors */
 
 #define PIN_LDR 17
+
+#define PIN_DHT 15
 
 /******************************************************************/
 
@@ -18,13 +21,30 @@
 /* Values */
 
 int value_LDR = 0;
-
+float value_humidity;
+float value_temperature;
 
 /* Constants */
 
 /******************************************************************/
+/**
+   Function prototypes
+*/
+void Init_SerialMonitor();
+void Loop_UploadAllData();
+
+void Init_PinModesAndSensors();
+
+void Loop_GatherData_LDR();
+void Loop_TakeDecision_LIGHTING(int ldr_value, bool isMotionDetected);
+
+bool Loop_GatherData_DHT11();
+void Loop_TakeDecision_DHT11();
+
+void Loop_UploadAllData();
 
 /* Initialization functions */
+DHT dht(PIN_DHT, DHT11);
 
 /**
    Initializes the Serial Monitor
@@ -37,11 +57,14 @@ void Init_SerialMonitor()
 /**
    Initializes pin modes and sensors
 */
-void Init_PinModesAndSensors_LDR()
+void Init_PinModesAndSensors()
 {
   //LDR init
   pinMode(PIN_LDR, INPUT);
   pinMode(PIN_LED_LDR, OUTPUT);
+  //DHT Init
+  pinMode(PIN_DHT, INPUT);
+  dht.begin();
 
 }
 
@@ -81,11 +104,69 @@ void Loop_TakeDecision_LIGHTING(int ldr_value, bool isMotionDetected)
   delay(1000);
 }
 
+/**
+   Read Data from the DHT11 Sensor
+
+*/
+bool Loop_GatherData_DHT11()
+{
+  value_humidity = dht.readHumidity();
+  value_temperature = dht.readTemperature();
+  if (isnan(value_humidity) || isnan(value_temperature))
+  {
+    Serial.println("Failed to read data from DHT11");
+    return false;
+  }
+  else
+  {
+    Serial.print("Temprature: ");
+    Serial.println(value_temperature);
+    Serial.print("Humidity: ");
+    Serial.println(value_humidity);
+    Serial.println();
+    return true;
+  }
+}
+
+void Loop_TakeDecision_DHT11()
+{
+  //Decision Temperature
+  if (value_temperature > 30)
+  {
+    Serial.println("Hot : Fan Speed -> Fast");
+  }
+  else if (value_temperature < 30 && value_temperature > 22)
+  {
+    Serial.println("Comfortable : Fan Speed -> Normal");
+  }
+  else
+  {
+    Serial.println("Cold : Fan Speed -> OFF");
+  }
+
+  //Decision Humidity
+  if (value_humidity < 80)
+  {
+    Serial.println("Comfortable : AC Temperature -> 27");
+  }
+  else
+  {
+    Serial.println("Too Humid : AC Temperature -> 25");
+  }
+  Serial.println();
+}
+
+
+void Loop_UploadAllData()
+{
+  //TODO implement
+}
+
 /* */
 
 void setup() {
   Init_SerialMonitor();
-  Init_PinModesAndSensors_LDR();
+  Init_PinModesAndSensors();
 
 
 }
@@ -93,4 +174,11 @@ void setup() {
 void loop() {
   Loop_GatherData_LDR();
   Loop_TakeDecision_LIGHTING(value_LDR, true);//
+
+  if (Loop_GatherData_DHT11())
+  {
+    Loop_TakeDecision_DHT11();
+  }
+
+  Loop_UploadAllData();
 }
